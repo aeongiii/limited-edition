dcd411fea262fab9f1c0335ebccf4e004c0d367f
aeongiii <ljhkys6874@naver.com>
2024-12-19 14:32:32 +0900

    init commit

---
+++ src/main/java/com/sparta/limited_edition/LimitedEditionApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.limited_edition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitedEditionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitedEditionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/limited_edition/LimitedEditionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.limited_edition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitedEditionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
