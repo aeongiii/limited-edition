8bf30ed6a35f3314bdbac988b0dec26e237281cd
aeongiii <ljhkys6874@naver.com>
2024-12-20 18:50:32 +0900

    [refactor] 예외처리 상태별 나누어 처리
    - GlobalExceptionHandler에서 400, 401, 500 예외처리
    - 로그인 실패 시 "이메일 또는 비밀번호가 올바르지 않습니다" 메시지 반환하는 InvalidCredentialsException 예외 따로 설정

---
+++ src/main/java/com/sparta/limited_edition/exception/InvalidCredentialsException.java
@@ -0,0 +1,12 @@
+package com.sparta.limited_edition.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// 로그인 - 이메일 또는 비밀번호가 올바르지 않습니다. (401)
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
