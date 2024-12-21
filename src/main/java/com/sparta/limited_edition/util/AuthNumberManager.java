10ce4c0d3abbe5fbb97d5c5ab4302cadbd51e705
aeongiii <ljhkys6874@naver.com>
2024-12-21 10:11:04 +0900

    [feat] 이메일 인증 시 인증번호를 5분간 Redis에 저장
    - 기존 Map에 저장하던 방식을 Redis로 변경
    - Key : 이메일, Value : 인증번호

--- src/main/java/com/sparta/limited_edition/util/AuthNumberManager.java
+++ src/main/java/com/sparta/limited_edition/util/AuthNumberManager.java
@@ -1,32 +1,40 @@
 package com.sparta.limited_edition.util;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 @Component
 public class AuthNumberManager {
-    // 이메일 인증번호를 임시로 ConcurrentHashMap에 저장. (key : 이메일주소, value : 인증번호)
-    // 추후 redis로 변경할 예정.
-    private final Map<String, String> authNumberMap = new ConcurrentHashMap<>();
-    // 스케줄링 (인증번호 유효시간 관리)
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public AuthNumberManager(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 
     // 인증번호 저장
     public void saveAuthNumber(String email, String authNumber, long validityInSeconds) {
-        authNumberMap.put(email, authNumber); // map에 이메일 + 인증번호 저장
-        // 유효시간 지나면 해당 이메일의 인증번호를 map에서 제거
-        scheduler.schedule(() -> authNumberMap.remove(email), validityInSeconds, TimeUnit.SECONDS);
-    }
+        // key에 이메일 저장
+        String redisKey = "auth:" + email;
+        // value에 인증번호 저장, 만료시간(시간, 단위) 설정
+        redisTemplate.opsForValue().set(redisKey, authNumber, validityInSeconds, TimeUnit.SECONDS);
+        System.out.println("Redis에 인증번호 저장 완료. key = " + redisKey + " value = " + authNumber);
+            }
 
     // 인증번호 검증
     public boolean validateAuthNumber(String email, String authNumber) {
-        // map에 해당 이메일 존재하는지 + 인증번호가 같은지 확인
-        return authNumberMap.containsKey(email) && authNumberMap.get(email).equals(authNumber);
+        // key 만들기
+        String redisKey = "auth:" + email;
+        // key값으로 value 가져오기
+        String redisValue = redisTemplate.opsForValue().get(redisKey);
+        // value가 없거나 다를때
+        if (redisValue == null || !redisValue.equals(authNumber)) {
+            System.out.println("인증번호 검증 실패");
+            return false;
+        }
+        System.out.println("인증번호 검증 성공");
+        return true;
     }
 }
